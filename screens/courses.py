3a80ad0d077de42a620a488395fe3ff2e849724a
Kevin <kt6037792@gmail.com>
2026-05-21 11:23:21 -0600

    Tarjetas dinámicas de cursos

--- screens/courses.py
+++ screens/courses.py
@@ -1,7 +1,9 @@
 from kivymd.uix.screen import MDScreen
 from kivy.lang import Builder
+from components.course_card import CourseCard
 
 KV = """
+
 <CoursesScreen>
 
     name: "courses"
@@ -17,23 +19,52 @@ KV = """
 
         ScrollView:
 
-            MDList:
-
-                OneLineListItem:
-                    text: "Curso de Panadería"
-
-                OneLineListItem:
-                    text: "Curso de Belleza"
-
-                OneLineListItem:
-                    text: "Curso de Computación"
-
-                OneLineListItem:
-                    text: "Curso de Inglés"
+            MDBoxLayout:
+                id: courses_box
+                orientation: "vertical"
+                adaptive_height: True
+                spacing: "20dp"
+                padding: "20dp"
 """
 
 Builder.load_string(KV)
 
 
 class CoursesScreen(MDScreen):
-    pass
+
+    def on_enter(self):
+
+        self.ids.courses_box.clear_widgets()
+
+        courses = [
+            {
+                "title": "Panadería",
+                "description": "Aprende elaboración de pan y repostería.",
+                "duration": "3 meses",
+            },
+            {
+                "title": "Belleza",
+                "description": "Curso profesional de estilismo.",
+                "duration": "4 meses",
+            },
+            {
+                "title": "Computación",
+                "description": "Ofimática y herramientas digitales.",
+                "duration": "2 meses",
+            },
+            {
+                "title": "Inglés",
+                "description": "Curso básico e intermedio.",
+                "duration": "6 meses",
+            },
+        ]
+
+        for course in courses:
+
+            card = CourseCard(
+                title=course["title"],
+                description=course["description"],
+                duration=course["duration"],
+            )
+
+            self.ids.courses_box.add_widget(card)
